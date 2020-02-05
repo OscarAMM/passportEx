e0586420400b9af8435a5ed29272e947ac7d2c8b
Oscar <oscar.monsreal1996@gmail.com>
2020-02-05 08:30:15 -0600

    URL changes

--- passport.php
+++ passport.php
@@ -56,7 +56,7 @@
                     <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Diplomado</a>
+                    <a class="nav-link" href="diplomat/sections.php">Diplomado</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Documentos</a>
