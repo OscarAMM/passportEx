614f78194b6a4b73b44a030d48e833bc475d2433
Oscar <oscar.monsreal1996@gmail.com>
2020-02-05 16:00:07 -0600

    Front page done

--- passport.php
+++ passport.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.6">
-    <title>Product example · Bootstrap</title>
+    <title>Pasaporte a la excelencia</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/product/">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
@@ -43,42 +43,171 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm fixed-top navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"><img src="img/logo2.png" alt="" width="50px"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03"
-            aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExample03">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="diplomat/sections.php">Diplomado</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Documentos</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-        <div class="col-md-5 p-lg-5 mx-auto my-5">
+    <div class="jumbotron p-3 p-md-5 m-md-3 text-center text-white" style="background-color: #0FB0DE">
+        <img src="img/logoSegey.png" width="250px" alt="" class="img-fluid float-left">
+        <div class="col-md-12 p-lg-5 mx-auto my-5">
             <h1 class="display-4 font-weight-normal">Pasaporte a la excelencia</h1>
-            <p class="lead font-weight-normal">Descripción breve del programa</p>
-            <a class="btn btn-outline-secondary" href="diplomat/sections.php">Ver</a>
+
+            <hr style="border: 1px solid white">
+            <h4 class="lead font-weight-normal">Estrategia integral</h4>
+            <a class="btn btn-outline-light" href="diplomat/sections.php">Ver</a>
         </div>
         <div class="product-device shadow-sm d-none d-md-block"></div>
         <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
     </div>
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-8 order-md-1 mb-4">
+                <img src="img/principal.png" alt="" class="img-fluid img-thumbnail" title="Pasaporte a la excelencia">
+                <h2 class="display-4"><b>Objetivo general</b></h2>
+                <hr style="border: 1px solid deepskyblue">
+                <p class="lead">Construye de manera integral los ambientes de aprendizaje de comunidades escolares que
+                    enfrentan mayores desafíos educativos, coordinado y concentrando con eficiencia los recursos
+                    disponibles para atender las necesidades identificadas, que conduzcan los procesos de aprendizaje
+                    hacia mejores resultados en pruebas estandarizadas.</p>
+                <p class="card-text">Es un conjunto de acciones coordinadas que pretende mejorar los procesos y
+                    resultados de los aprendizajes de los niños, las niñas y los/las adolescentes. La participación de
+                    las escuelas es voluntaria, por decisión del colectivo docente
+                    y que responda a la invitación de la <b>Dirección General de Desarrollo Educativo y Gestión
+                        Regional.</b></p>
+            </div>
+            <div class="col-md-4 order-md-2 text-center text-white box shadow mb-4" style="background-color:#0FB0DE">
+                <!---- IMG 1 --->
+                <img src="img/lateral2.png" alt="logo" class="img-fluid img-thumbnail" width="250px"
+                    style="margin-top:10px" title="comunidad escolar">
+                <h2>Ámbito de la comunidad escolar</h2>
+                <p>Atiende a directivos y docentes</p>
+                <!----- IMG 2 --->
+                <img src="img/lateral3.png" alt="logo" class="img-fluid img-thumbnail" width="250px"
+                    title="Ámbito familiar">
+                <h2>Ámbito familiar</h2>
+                <p>Atiende a madres y padres de familia</p>
+                <!----IMG 3---->
+                <img src="img/lateral4.png" alt="logo" class="img-fluid img-thumbnail" width="250px"
+                    title="Organizaciones alianza">
+                <h2>Organizaciones Alianza</h2>
+                <p>Apoya a toda la familia y la comunidad escolar con servicios complementarios</p>
+            </div>
+        </div><!-- /.row -->
+        <div class="row mb-4">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;background-color: #00BFFF">
+                    <img src="img/1.png" alt="" class="img-fluid mx-auto d-block rounded-circle" width="140px"
+                        style="margin-top:10px">
+                    <div class="card-body">
+                        <h3 class="text-center">SITUACIÓN ACTUAL DE LA ESCUELA</h3>
+                        <p class="card-text text-center">Se trabaja atendiendo las principales necesidades escolares.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;background-color: #20B2AA; height:347px">
+                    <img src="img/2.png" alt="" class="img-fluid mx-auto d-block rounded-circle" width="140px"
+                        style="margin-top:10px">
+                    <div class="card-body">
+                        <h3 class="text-center">DIPLOMADO EN TU ESCUELA</h3>
+                        <p class="card-text text-center">Se implementa una formacón docente personalizada.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem; background-color: #90EE90; height:347px">
+                    <img src="img/3.png" alt="" class="img-fluid  mx-auto d-block  rounded-circle" width="140px"
+                        style="margin-top:10px">
+                    <div class="card-body">
+                        <h3 class="text-center">EXAMEN IDAEPY</h3>
+                        <p class="card-text text-center">Se aplica el examen IDAEPY para reconocer avances</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="lead">La estrategia de <i>"Pasaporte a la Excelencia"</i> recibirá el apoyo de instituciones
+                    educativas, dependencias gubernamentales, programas académicos y organzaciones de la sociedad civil
+                    como aliados estratégicos de la SEGEY.</p>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <h2 class=" font-weight-bold text-primary">DIPLOMADO "PASAPORTE A LA EXCELENCIA"</h2>
+                <p class="lead">Objetivo</p>
+                <p class="lead font-italic">"El docente, durante el diplomado, es capaz de incrementar de mnaera
+                    progresiva e incluyente el aprendizaje de sus estudiantes en español y matemáticas mediante una
+                    propuesta innovadora de enseñanza orientada hacia la mejora de los niveles de logro en las
+                    puebras nacionales y locales"</p>
+                <p class="font-weight-normal">El diplomado está organizado en 4 módulos</p>
+                <ul class="card-text">
+                    <li>Autoreflexión sobre la práctica docente/directiva</li>
+                    <li>Habilidades para la vida</li>
+                    <li>Procesos de aprendizaje y enseñanza de las matemáticas</li>
+                    <li>Procesos de aprendizaje y enseñanza del español</li>
+                </ul>
+                <p class="font-weight-normal">
+                    Tendrá una duración de 120 horas, dividido en:
+                </p>
+                <ul>
+                    <li>40 presenciales, impartido en cada escuela en
+                        horarios y días acordados por los docentes y
+                        directivos, de enero a julio del 2020</li>
+                    <li>80 de trabajo independiente en el aula, donde
+                        la maestra/o pone en práctica los aprendizajes
+                        alcanzados.</li>
+                </ul>
+                <p class="font-weight-normal">
+                    Se desarrolla a partir de la integración de los
+                    ámbitos de la comunidad escolar y la
+                    comunidad familiar, que se interrelacionan en
+                    los cuatro módulos. Conecta la teoría
+                    psicopedagógica y la práctica docente,
+                    desarrollándose con base en metodologías
+                    activas de aprendizaje y fortalecimiento de las
+                    habilidades para la vida, promoviendo el
+                    desarrollo personal y profesional de los
+                    docentes y directivos, para la mejora de sus
+                    prácticas docentes y de los resultados de
+                    aprendizaje de sus estudiantes.
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img src="img/lateral.png" alt="" class="img-fluid img-thumbnail box shadow mb-4">
+                <div class="card mb-4 box shadow">
+                    <img src="img/comunidad.png" class="card-img-top img-fluid" alt="comunidad" title="comunidad-escuela" >
+                    <div class="card-body">
+                        <h5 class="card-title">Escuela-Comunidad</h5>
+                        <p class="lead">Se promoverá un mayor acceso a servicios complementarios para el
+                            desarrollo
+                            social y
+                            económico. Esto se efectuará a través de la vinculación de la SEGEY con otras
+                            organizaciones
+                            e
+                            instutuciones gubernamentrales, empresariales y no gubernamentales y de la sociedad
+                            civil.
+                        </p>
+                    </div>
+                </div>
+                <div class="card box shadow p-3 text-right">
+                    <blockquote class="blockquote mb-0">
+                        <p>"La enseñanza no se hace de cabeza a cabeza, sino de corazón a corazón"</p>
+                        <footer class="blockquote-footer">
+                            <small class="text-muted">
+                                <cite title="Source Title">Howard G.Hendricks</cite>
+                            </small>
+                        </footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <footer class="container py-5">
+        <hr>
         <div class="row">
             <div class="col-12 col-md">
-                <img src="img/logo2.png"  width="50px" alt="">
+                <img src="img/logo2.png" width="50px" alt="">
                 <small class="d-block mb-3 text-muted">&copy; 2019-2020</small>
             </div>
             <div class="col-6 col-md">
