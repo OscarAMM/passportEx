46840fc51f5cc341cee0b3a5fae14cf3869b235a
Oscar <oscar.monsreal1996@gmail.com>
2020-02-06 09:45:20 -0600

    New colors added at css and new format to section.php

--- diplomat/sections.php
+++ diplomat/sections.php
@@ -14,7 +14,7 @@
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js">
     </script>
-    <script src="../css/button-color.css"></script>
+    <link rel="stylesheet" href="../css/button-color.css">
     <title>SEGEY | PE</title>
 </head>
 
@@ -34,50 +34,55 @@
         <hr style="border: 1px solid deepskyblue">
         <div class="row">
             <div class="col-md-4">
-                <div class="card box shadow mb-4">
+                <div class="card box shadow mb-4 text-white" style="background-color:#FF8509">
                     <div class="card-body">
                         <h5 class="card-title">Procesos de aprendizaje y enseñanza de las matemáticas </h5>
+                        <hr style="border: 1px solid white">
                         <p class="card-text"></p>
-                        <p><a href="" class="btn btn-primary">Ver</a></p>
+                        <p><a href="" class="btn btn-section-1">Acceder</a></p>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card box shadow">
+                <div class="card box shadow text-white" style="background-color:#FF8509">
                     <div class="card-body">
                         <h5 class="card-title">Procesos de aprendizaje y enseñanza del español</h5>
+                        <hr style="border: 1px solid white">
                         <p class="card-text"></p>
-                        <p><a href="" class="btn btn-primary">Ver</a></p>
+                        <p><a href="" class="btn btn-section-1">Acceder</a></p>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card box shadow mb-4">
+                <div class="card box shadow mb-4 text-white"style="background-color:#FF8509">
                     <div class="card-body">
                         <h5 class="card-title">Auto reflexión sobre la práctica docente/directiva.</h5>
+                        <hr style="border: 1px solid white">
                         <p class="card-text"></p>
-                        <p><a href="" class="btn btn-primary">Ver</a></p>
+                        <p><a href="" class="btn btn-section-1">Acceder</a></p>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-md-4">
-                <div class="card box shadow mb-4">
+                <div class="card box shadow mb-4 text-white" style="background-color:#FF8509">
                     <div class="card-body">
                         <h5 class="card-title">Documentos de evidencia</h5>
+                        <hr style="border: 1px solid white">
                         <p class="card-text"></p>
-                        <p><a href="evidence_docs/sub_sections.php" class="btn btn-primary">Ver</a></p>
+                        <p><a href="evidence_docs/sub_sections.php" class="btn btn-section-1">Acceder</a></p>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-4">
-                <div class="card box shadow mb-4">
+                <div class="card box shadow mb-4 text-white" style="background-color:#FF8509">
                     <div class="card-body">
                         <h5 class="card-title">Habilidades para la vida</h5>
+                        <hr style="border: 1px solid white">
                         <p class="card-text"></p>
-                        <p><a href="" class="btn btn-primary">Ver</a></p>
+                        <p><a href="" class="btn btn-section-1">Acceder</a></p>
                     </div>
                 </div>
             </div>
