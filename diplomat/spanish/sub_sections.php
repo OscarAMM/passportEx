1c8fbccd9b9929e2bcae5e97b5b36ae199ff95d9
Oscar <oscar.monsreal1996@gmail.com>
2020-02-13 12:07:27 -0600

    New section for PAEX

---
+++ diplomat/spanish/sub_sections.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/product/">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="../../css/button-color.css">
+    <title>SEGEY | Práctica</title>
+</head>
+
+<body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal"><a href="../../passport.php" class="btn font-weight-bold"><img
+                    src="../../img/logo2.png" class="img-fluid" width="40px" alt="">PAEX</a></h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Matemáticas</a>
+            <a class="p-2 text-dark" href="#">Español</a>
+            <a class="p-2 text-dark" href="#">Práctica docente</a>
+            <a class="p-2 text-dark" href="#">Habilidades para la vida</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="#">Iniciar sesión</a>
+    </div>
+    <div class="container">
+        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+            <h1 class=" text-center">PASAPORTE A LA EXCELENCIA </h1>
+            <h4>Procesos de aprendizaje y eneseñanza del español</h4>
+            <p class="lead">Podrás consultar el programa, así como el contenido de la misma además de poder descargar
+                las actividades.</p>
+            <hr>
+        </div>
+        <div class="card-deck mb-3 text-center">
+            <div class="card box shadow mb-4 text-white" style=" background-color:#00BFFF;">
+                <div class="card-header">
+                    <h4 class="card-title">Evidencia</h4>
+                </div>
+                <div class="card-body">
+                    <p class="lead">Descarga docs. y fotos</p>
+                    <a href="../evidence_docs/sub_sections.php" class="btn btn-section-1">Acceder</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
